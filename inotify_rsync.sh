609b5319e124a78d9f1240b42c86efa3d11ad1f0
Mose KE <eric24831684@gmail.com>
2021-10-18 11:49:03 +0800

    > remove inotify event, left create,delete only. > adjust rsync args to speed up! (ssh).

--- inotify_rsync.sh
+++ inotify_rsync.sh
@@ -7,15 +7,15 @@ rsync_module='rocket_sync'
 password_file='/root/rocketchat/rsync.passwd'
 
 
-/usr/bin/inotifywait -mrq --format '%w%f %e' -e create,close_write,delete,modify $monitor_path | while read line 
+/usr/bin/inotifywait -mrq --format '%w%f %e' -e create,delete $monitor_path | while read line 
 do
     file=$(echo ${line} | awk '{print $1}')
     event=$(echo ${line} | awk '{print $2}')
     dir="$(dirname ${file})/"
 
     if [ ${event} != 'DELETE' ];then
-        rsync -av --delete --rsh=ssh ${file}  $rsync_user@$remote_server1::$rsync_module/ --password-file=$password_file --log-file=/var/log/rsync_inotify.log
-        rsync -av --delete --rsh=ssh ${file}  $rsync_user@$remote_server2::$rsync_module/ --password-file=$password_file --log-file=/var/log/rsync_inotify.log
+        rsync -avP --rsh=ssh -e "ssh -x -T -o Compression=no" ${file}  $rsync_user@$remote_server1::$rsync_module/ --password-file=$password_file --log-file=/var/log/rsync_inotify.log
+        rsync -avP --rsh=ssh -e "ssh -x -T -o Compression=no" ${file}  $rsync_user@$remote_server2::$rsync_module/ --password-file=$password_file --log-file=/var/log/rsync_inotify.log
     fi
 
     if [ ${event} == 'DELETE' ];then
